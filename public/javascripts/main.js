47f1206b9a0940e38bfd26ae838e58bb52fd45a3
SirJeany <jmvanriet1@gmail.com>
2020-04-13 15:32:40 +0200

    added rune-selection functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,3 +2,18 @@
 
 // When a rune is clicked, it should expand and stay that way - the reverse should also happen
 
+let runeImgs = document.querySelectorAll('.runeImgs');
+
+function selectRune() {
+    // Check if this rune has been selected:
+    if(this.classList.contains('runeImgClicked')) {
+        // if so, remove it.
+        this.classList.remove('runeImgClicked');
+    } else {
+        this.classList.add('runeImgClicked');
+    }
+}
+
+runeImgs.forEach(rune => {
+    rune.addEventListener('click', selectRune);
+});
