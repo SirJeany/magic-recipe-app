95268b21a0fa0aae723f857a4a3e94ef98a60537
SirJeany <jmvanriet1@gmail.com>
2020-04-13 15:44:03 +0200

    Added hover shadows for the svg runes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,14 +3,17 @@
 // When a rune is clicked, it should expand and stay that way - the reverse should also happen
 
 let runeImgs = document.querySelectorAll('.runeImgs');
+let rune_select_counter = 0;
 
 function selectRune() {
     // Check if this rune has been selected:
     if(this.classList.contains('runeImgClicked')) {
         // if so, remove it.
         this.classList.remove('runeImgClicked');
+        rune_select_counter--;
     } else {
         this.classList.add('runeImgClicked');
+        rune_select_counter++;
     }
 }
 
