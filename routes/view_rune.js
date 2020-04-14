95f8d885f9f9eec762d36732b7c64d8b777407a1
SirJeany <jmvanriet1@gmail.com>
2020-04-14 16:05:26 +0200

    Views working

--- routes/view_rune.js
+++ routes/view_rune.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Runes = require('../runebd.json');
 
-router.get('/runeId', function(req, res, next){
+router.get('/:runeId', function(req, res, next){
+    console.log(JSON.stringify(Runes.Found[req.params.runeId-1]));
     let data = {
         title: "Well Done!",
-        rune: Runes.Found[req.params.runeId]
+        rune: Runes.Found[req.params.runeId-1]
     }
     res.render('view_rune', data);
 });
